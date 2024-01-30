0bae75b7e9a2fa98221ad493229a057de6773978
rakapp17 <rakap17@gmail.com>
2024-01-30 19:56:18 +0700

    fix error

--- src/app/components/Connect.tsx
+++ src/app/components/Connect.tsx
@@ -5,7 +5,7 @@ function Connect() {
     <div className="w-full h-[646px] bg-[url('/hero7.jpg')] bg-cover flex items-center justify-center backdrop-blur-lg backdrop-brightness-50 bg-center">
       <div className="items-center justify-center">
         <h2 className=" text-white text-center text-[30px] font-bold mb-2">
-          Let's Connect
+          Lets&apos; Connect
         </h2>
         <button className="text-white px-3 py-2 border-2 border-white rounded-xl self-center ml-[8px] hover:bg-white hover:text-slate-600">
           Download Pricelist
