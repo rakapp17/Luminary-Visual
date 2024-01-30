790ff973f987b1d3f68f16fe48f56322ec0cd7fe
rakapp17 <rakap17@gmail.com>
2024-01-30 22:30:44 +0700

    change image on portofolio

--- src/app/portofolio/page.tsx
+++ src/app/portofolio/page.tsx
@@ -3,6 +3,7 @@ import Navbarabu from "../components/Navbarabu";
 import Portofolio from "../components/Portofolio";
 import Blogporto from "../components/Blogporto";
 import Image from "next/image";
+import image04 from "@/app/hero4.png";
 function page() {
   return (
     <div>
@@ -12,7 +13,7 @@ function page() {
       <div className="w-full h-svh relative">
         <div className="absolute inset-0">
           <Image
-            src={"/hero4.png"}
+            src={image04}
             alt="background image"
             fill
             className="object-cover bg-center"
